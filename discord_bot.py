e61c4c51dec94e806dce754de06fb57db6065263
BenceGamerYT <bodobence36@gmail.com>
2025-11-10 12:08:14 +0100

    visszaraktam alaphelyzetbe, tokeletes

--- discord_bot.py
+++ discord_bot.py
@@ -13,6 +13,14 @@ intents.members = True
 # Bot prefix (pl. !kick, !ban stb.)
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Esemény: Bot készen áll
+@bot.event
+async def on_ready():
+    print(f"✅ Bejelentkezve: {bot.user}")
+
+
+bot.run("MTQzNTY2MTI0MzM1MTMwMjIzNQ.GtqXh2.pt5AUuz7uBh_rpOGxthH0qztry5a3VyR__gahE")
+
 # --- MODERÁCIÓS PARANCSOK (csak Rendszergazda jogosultsággal) ---
 
 def admin_or_role(ctx):
